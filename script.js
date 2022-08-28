aa366b0888dce075c34370530dc79063a52b1e22
lifeconsciousness <lifeconsciousness321@gmail.com>
2022-08-29 01:53:25 +0200

    initial commit/added navbar

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const navbar = document.querySelector('.navbar')
+const navbarToggle = document.querySelector('.mobile-navbar-toggle')
+
+navbarToggle.addEventListener('click', ()=> {
+    const visibility = navbar.getAttribute('data-visible')
+    
+    if(visibility === "false"){
+        navbar.setAttribute('data-visible', true)
+        navbarToggle.setAttribute('aria-expanded', true)
+    } else{
+        navbar.setAttribute('data-visible', false)
+        navbarToggle.setAttribute('aria-expanded', false)
+    }
+})
