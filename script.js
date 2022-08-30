1674f1144afb4bf1ba6f9cb8323ca873771d741a
lifeconsciousness <lifeconsciousness321@gmail.com>
2022-08-30 02:11:18 +0200

    navbar and language button styling mostly finished

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//navigation bar toggle on phone devises
 const navbar = document.querySelector('.navbar')
 const navbarToggle = document.querySelector('.mobile-navbar-toggle')
 
@@ -13,6 +13,28 @@ navbarToggle.addEventListener('click', ()=> {
         navbarToggle.setAttribute('aria-expanded', false)
     }
 })
+
+//language choice button click on mobile
+
+const languageIcon = document.querySelector(".selected-lang")
+let langListToggle = false
+
+languageIcon.addEventListener('click', ()=>{
+    if(!langListToggle){
+        document.getElementById("languages").style.display = "block";
+        langListToggle = true
+    }else if(langListToggle){
+        document.getElementById("languages").style.display = "none";
+        langListToggle = false
+    }
+})
+
+navbarToggle.addEventListener('click', ()=>{
+    document.getElementById("languages").style.display = "none";
+})
+
+
+
 
 
 
